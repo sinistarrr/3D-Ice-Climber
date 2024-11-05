b21120e5561160eaddfe786875064b8ea3b1158a
sinistarrr <ludcat77@hotmail.fr>
2024-11-05 18:53:23 +0100

    Made NPCs spawn at the right places

--- 3D-Ice-Climber/Assets/Scripts/PlayerController.cs
+++ 3D-Ice-Climber/Assets/Scripts/PlayerController.cs
@@ -7,10 +7,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput, verticalInput, jumpInput, fireInput;
-    public float speed = 5.0f;
+    public float speed = 5.5f;
     public float jumpForce = 14.0f;
     private float enemyPushForce = 20.0f;
-    private float xBound = 17.0f; 
+    private float xBound = 17.0f;
     private bool isJumping = false;
     private bool isOnGround = false;
     private bool jumpingKeyIsReleased = true;
@@ -35,7 +35,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(!gameOver){
+        if (!gameOver)
+        {
             // At each frame we check if the player is jumping, the user jump input, all depending whether 
             // the player is on the ground or not
             PlayerCheckJumpingState();
@@ -45,12 +46,14 @@ public class PlayerController : MonoBehaviour
             LockPlayerControls();
         }
         ConstraintPlayerPosition();
-        
+
     }
 
-    void FixedUpdate(){
+    void FixedUpdate()
+    {
         // if jumping is detected and player is on the ground, the player goes up on the Y-Axis with a force
-        if(isJumping && isOnGround){
+        if (isJumping && isOnGround)
+        {
             Jump();
             isJumping = false;
             StartCoroutine(JumpCooldown());
@@ -58,22 +61,27 @@ public class PlayerController : MonoBehaviour
             horizontalInput /= 2;
         }
         //Debug.Log(collisions);
-        if(collisions == 0 || (collisions > 0 && !collidingWithGround)){
+        if (collisions == 0 || (collisions > 0 && !collidingWithGround))
+        {
             isOnGround = false;
         }
-        else{
+        else
+        {
             isOnGround = true;
         }
     }
 
-    private void OnCollisionEnter(Collision collision){
+    private void OnCollisionEnter(Collision collision)
+    {
         // Never returns null because of "root"
         GameObject parentOfGameObjectCollidedWith = collision.transform.root.gameObject;
         collisions++;
 
-        if(!gameOver && (parentOfGameObjectCollidedWith.CompareTag("Ground Breakable") || parentOfGameObjectCollidedWith.CompareTag("Ground Breakable Small"))){
+        if (!gameOver && (parentOfGameObjectCollidedWith.CompareTag("Ground Breakable") || parentOfGameObjectCollidedWith.CompareTag("Ground Breakable Small")))
+        {
             // Destruction of Block
-            if((Math.Abs(collision.contacts[0].point.y - (transform.position.y + GetComponent<Collider>().bounds.size.y)) <= 0.05f) && (Math.Round(collision.contacts[0].point.y, 2) == Math.Round(parentOfGameObjectCollidedWith.transform.position.y - collision.gameObject.GetComponent<Collider>().bounds.size.y / 2, 2))){
+            if ((Math.Abs(collision.contacts[0].point.y - (transform.position.y + GetComponent<Collider>().bounds.size.y)) <= 0.05f) && (Math.Round(collision.contacts[0].point.y, 2) == Math.Round(parentOfGameObjectCollidedWith.transform.position.y - collision.gameObject.GetComponent<Collider>().bounds.size.y / 2, 2)))
+            {
                 // Debug.Log("collision.contacts[0].point = " + collision.contacts[0].point);
                 // Debug.Log("collision.transform.position = " + collision.transform.position);
                 // Debug.Log("collision.gameObject.GetComponent<Collider>().bounds.size = " + collision.gameObject.GetComponent<Collider>().bounds.size);
@@ -84,15 +92,18 @@ public class PlayerController : MonoBehaviour
                 collisions--;
             }
         }
-        if(!gameOver && (parentOfGameObjectCollidedWith.CompareTag("Ground Breakable") || parentOfGameObjectCollidedWith.CompareTag("Ground Breakable Small") || parentOfGameObjectCollidedWith.CompareTag("Ground"))){
+        if (!gameOver && (parentOfGameObjectCollidedWith.CompareTag("Ground Breakable") || parentOfGameObjectCollidedWith.CompareTag("Ground Breakable Small") || parentOfGameObjectCollidedWith.CompareTag("Ground")))
+        {
             Debug.Log("Math.Round(collision.contacts[0].point.y, 1) = " + Math.Round(collision.contacts[0].point.y, 1));
             Debug.Log("Math.Round(parentOfGameObjectCollidedWith.transform.position.y + collision.gameObject.GetComponent<Collider>().bounds.size.y / 2, 2) = " + Math.Round(parentOfGameObjectCollidedWith.transform.position.y + collision.gameObject.GetComponent<Collider>().bounds.size.y / 2, 1));
             Debug.Log("collision.gameObject.GetComponent<Collider>().bounds.size = " + collision.gameObject.GetComponent<Collider>().bounds.size);
             Debug.Log("collision.gameObject.GetComponent<Collider>().bounds.size.y = " + collision.gameObject.GetComponent<Collider>().bounds.size.y);
             Debug.Log("collision.gameObject.GetComponent<Collider>().bounds.size.y / 2 " + collision.gameObject.GetComponent<Collider>().bounds.size.y / 2);
-            if(Math.Abs(Math.Round(collision.contacts[0].point.y, 1) - Math.Round(parentOfGameObjectCollidedWith.transform.position.y + collision.gameObject.GetComponent<Collider>().bounds.size.y / 2, 1)) <= 0.1f){
+            if (Math.Abs(Math.Round(collision.contacts[0].point.y, 1) - Math.Round(parentOfGameObjectCollidedWith.transform.position.y + collision.gameObject.GetComponent<Collider>().bounds.size.y / 2, 1)) <= 0.1f)
+            {
                 collidingWithGround = true;
-                if(!jumpCooldownElapsed){
+                if (!jumpCooldownElapsed)
+                {
                     jumpCooldownElapsed = true;
                     playerAnim.enabled = true;
                     playerAnim.Rebind();
@@ -101,11 +112,13 @@ public class PlayerController : MonoBehaviour
                     ManageHorizontalAnimation();
                 }
             }
-            else{
+            else
+            {
                 collidingWithGround = false;
             }
         }
-        if(collision.gameObject.CompareTag("Chicken")){
+        if (collision.gameObject.CompareTag("Chicken"))
+        {
 
             // If the object we hit is the enemy
             // Calculate Angle Between the collision point and the player
@@ -124,30 +137,36 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void OnCollisionExit(Collision collision) {
+    private void OnCollisionExit(Collision collision)
+    {
         collisions--;
     }
 
     // Player jumping state control
-    private void PlayerCheckJumpingState(){
+    private void PlayerCheckJumpingState()
+    {
         // if the player is not already jumping, is on the ground, released the jump key, and is pressing jump
-        if(!isJumping && isOnGround && jumpingKeyIsReleased && jumpInput > 0){
+        if (!isJumping && isOnGround && jumpingKeyIsReleased && jumpInput > 0)
+        {
             isJumping = true;
             jumpingKeyIsReleased = false;
             ManageJumpingAnimation();
         }
         // verification of whether the player has released the jump key or not to avoid jumping in loop
-        else if(!jumpingKeyIsReleased && jumpInput == 0 && isOnGround){
+        else if (!jumpingKeyIsReleased && jumpInput == 0 && isOnGround)
+        {
             jumpingKeyIsReleased = true;
         }
     }
 
-    private void Jump(){
+    private void Jump()
+    {
         playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 
     // reads input of the player on X axis, Y axis and jump input for both keyboard and controller
-    private void InputAxisReadings(){
+    private void InputAxisReadings()
+    {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         fireInput = Input.GetAxisRaw("Fire1");
         FireInputAnimation();
@@ -157,80 +176,100 @@ public class PlayerController : MonoBehaviour
         ManageCrouchingAnimation();
         jumpInput = Input.GetAxisRaw("Jump");
     }
-    
+
     // provides horizontal movement to the player
-    private void MovePlayerHorizontally(){
-        if(!isCrouching){
+    private void MovePlayerHorizontally()
+    {
+        if (!isCrouching)
+        {
             playerRb.MovePosition(transform.position + Vector3.right * horizontalInput * speed * Time.deltaTime);
         }
     }
 
     // locks some of the controls of the player when the player isn't on the ground (we stop reading the inputs)
     // unless he is in not on the ground, standing still, and descending towards the ground.
-    private void LockPlayerControls(){
-        if(isOnGround || (!isOnGround && (horizontalInput == 0) && (playerRb.velocity.y <= 0))){
+    private void LockPlayerControls()
+    {
+        if (isOnGround || (!isOnGround && (horizontalInput == 0) && (playerRb.velocity.y <= 0)))
+        {
             InputAxisReadings();
         }
     }
 
     // X axis bound checking to prevent player from going out of bounds
-    private void ConstraintPlayerPosition(){
-        if(transform.position.x < -xBound){
+    private void ConstraintPlayerPosition()
+    {
+        if (transform.position.x < -xBound)
+        {
             transform.position = new Vector3(xBound, transform.position.y, transform.position.z);
         }
-        else if(transform.position.x > xBound){
+        else if (transform.position.x > xBound)
+        {
             transform.position = new Vector3(-xBound, transform.position.y, transform.position.z);
         }
     }
 
-    private void HorizontalDirectionInversion(){
-        if(((horizontalInput < 0 && isMovingRight) || (horizontalInput > 0 && !isMovingRight)) && !isCrouching){
+    private void HorizontalDirectionInversion()
+    {
+        if (((horizontalInput < 0 && isMovingRight) || (horizontalInput > 0 && !isMovingRight)) && !isCrouching)
+        {
             InversePlayerRotation();
         }
     }
 
-    private void ManageHorizontalAnimation(){
-        if((horizontalInput == 0 && isMovingHorizontally) || isCrouching){
+    private void ManageHorizontalAnimation()
+    {
+        if ((horizontalInput == 0 && isMovingHorizontally) || isCrouching)
+        {
             playerAnim.SetFloat("Speed_f", 0);
             isMovingHorizontally = false;
         }
-        else if(horizontalInput != 0 && !isMovingHorizontally){
+        else if (horizontalInput != 0 && !isMovingHorizontally)
+        {
             playerAnim.SetBool("Static_b", true);
             playerAnim.SetFloat("Speed_f", 0.75f);
             isMovingHorizontally = true;
         }
-        
+
     }
-    
-    private void FireInputAnimation(){
-        if(fireInput != 0 && !isFiring){
+
+    private void FireInputAnimation()
+    {
+        if (fireInput != 0 && !isFiring)
+        {
             playerAnim.SetInteger("WeaponType_int", 10);
             isFiring = true;
             StartCoroutine(FiringCooldown());
         }
     }
 
-    private void ManageJumpingAnimation(){
+    private void ManageJumpingAnimation()
+    {
         playerAnim.SetTrigger("Jump_trig");
     }
 
-    private void ManageCrouchingAnimation(){
-        if(verticalInput != 0 && !isCrouching && !isJumping){
+    private void ManageCrouchingAnimation()
+    {
+        if (verticalInput != 0 && !isCrouching && !isJumping)
+        {
             playerAnim.SetBool("Crouch_b", true);
             isCrouching = true;
         }
-        else if(verticalInput == 0 && isCrouching){
+        else if (verticalInput == 0 && isCrouching)
+        {
             playerAnim.SetBool("Crouch_b", false);
             isCrouching = false;
         }
     }
 
-    private void InversePlayerRotation(){
+    private void InversePlayerRotation()
+    {
         isMovingRight = !isMovingRight;
         transform.rotation = Quaternion.Inverse(transform.rotation);
     }
 
-    private void ManageDeathAnimation(){
+    private void ManageDeathAnimation()
+    {
         playerAnim.SetBool("Death_b", true);
         playerAnim.SetInteger("DeathType_int", 2);
     }
@@ -246,7 +285,7 @@ public class PlayerController : MonoBehaviour
     {
         yield return new WaitForSeconds(0.2f);
         jumpCooldownElapsed = false;
-        
+
     }
-    
+
 }
--- 3D-Ice-Climber/Assets/Scripts/SpawnManager.cs
+++ 3D-Ice-Climber/Assets/Scripts/SpawnManager.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
-{   
+{
     public GameObject unbreakableBlockPrefab;
     public GameObject breakableBlockPrefab;
     public GameObject smallBreakableBlockPrefab;
@@ -13,7 +13,6 @@ public class SpawnManager : MonoBehaviour
     public GameObject iceFallingPrefab;
     private Vector3 originBlockPosition = new Vector3(-16.5f, -2.0f, -0.5f);
     private Vector3 unbreakableBlockPrefabBounds, breakableBlockPrefabBounds, smallBreakableBlockPrefabBounds;
-    private Vector3 originalBlockLocalScale;
     private float rowHeight = 3.75f;
     private int rowCount = 8;
     private int currentRowCount = 5;
@@ -21,19 +20,20 @@ public class SpawnManager : MonoBehaviour
     private List<Vector3> breakableBlocksPrefabsBounds;
     private List<GameObject> breakableBlocksPrefabs;
     private List<List<GameObject>> listOfGrounds;
-    private float xBound = 10.5f; 
-    private int chickensNumberLimit = 3;
+    private float xBound = 17.0f;
+    private int chickensNumberLimit = 2;
+    private float spawnManagerPositionYOffset = 1.75f;
     // Start is called before the first frame update
     void Start()
     {
 
         BoundsVariablesInit();
         // be careful that both lists have same prefab order, its important for later
-        breakableBlocksPrefabs = new List<GameObject> {breakableBlockPrefab, smallBreakableBlockPrefab};
-        breakableBlocksPrefabsBounds = new List<Vector3> {breakableBlockPrefabBounds, smallBreakableBlockPrefabBounds};
+        breakableBlocksPrefabs = new List<GameObject> { breakableBlockPrefab, smallBreakableBlockPrefab };
+        breakableBlocksPrefabsBounds = new List<Vector3> { breakableBlockPrefabBounds, smallBreakableBlockPrefabBounds };
         ListOfGroundsInit();
         // Creation of the blocks in the game
-        
+
         // Spawn manager of the chickens
         StartCoroutine(SpawnChickenPeriodically());
     }
@@ -44,46 +44,73 @@ public class SpawnManager : MonoBehaviour
 
     }
 
-    private void SpawnChicken(){
-        int numberOfChickenObjects = GameObject.FindGameObjectsWithTag("Chicken").Length;
+    private void SpawnChicken()
+    {
+        List<GameObject> gameObjects = GameObject.FindGameObjectsWithTag("Chicken").ToList();
+        int numberOfChickenObjects = gameObjects.Count();
 
-        if(numberOfChickenObjects < chickensNumberLimit){
-            Vector3 spawnPos = new Vector3(-xBound, transform.position.y, transform.position.z - 0.5f);
-            if(Random.Range(0, 2) == 0){
-                Instantiate(chickenPrefab, spawnPos, chickenPrefab.transform.rotation);
-            }
-            else{
-                Instantiate(chickenPrefab, spawnPos, Quaternion.Inverse(chickenPrefab.transform.rotation));
+        if (numberOfChickenObjects < chickensNumberLimit)
+        {   
+            int lineToSpawnChickenOn = GetLineToSpawnChickenOn(gameObjects, numberOfChickenObjects);
+            if(lineToSpawnChickenOn != -1){
+                Vector3 spawnPos = new Vector3(-xBound, transform.position.y - spawnManagerPositionYOffset + (lineToSpawnChickenOn * rowHeight), transform.position.z - 0.5f);
+                if (Random.Range(0, 2) == 0)
+                {
+                    Instantiate(chickenPrefab, spawnPos, chickenPrefab.transform.rotation);
+                }
+                else
+                {
+                    Instantiate(chickenPrefab, spawnPos, Quaternion.Inverse(chickenPrefab.transform.rotation));
+                }
             }
         }
     }
 
+    private int GetLineToSpawnChickenOn(List<GameObject> gameObjects, int numberOfChickenObjects){
+        float firstLineYPos = transform.position.y - spawnManagerPositionYOffset;
+
+        // List of possible lines we want to spawn chickens on
+        List<int> possibleLinesToSpawnChickenOn = new List<int>(){currentRowCount - (2 + currentRowCount % 2), currentRowCount - (4 + currentRowCount % 2)};
+        if(numberOfChickenObjects == 0){
+            return possibleLinesToSpawnChickenOn[Random.Range(0, possibleLinesToSpawnChickenOn.Count())];
+        }
+        else{
+            for(int i = 0; i < possibleLinesToSpawnChickenOn.Count(); i++){
+                if(!gameObjects.Find(chicken => System.Math.Abs(chicken.transform.position.y - (firstLineYPos + rowHeight * possibleLinesToSpawnChickenOn[i])) < 1)){
+                    return possibleLinesToSpawnChickenOn[i];
+                }
+            }
+            return -1;
+        }
+    }
     private IEnumerator SpawnChickenPeriodically()
     {
         int waitingTime;
 
-        while(true){
+        while (true)
+        {
             waitingTime = Random.Range(4, 11);
 
             yield return new WaitForSeconds(waitingTime);
 
             SpawnChicken();
         }
-        
+
     }
 
-    private void BoundsVariablesInit(){
+    private void BoundsVariablesInit()
+    {
         GameObject unb = Instantiate(unbreakableBlockPrefab), br = Instantiate(breakableBlockPrefab), smbr = Instantiate(smallBreakableBlockPrefab);
         unbreakableBlockPrefabBounds = unb.GetComponentInChildren<Collider>().bounds.size;
         breakableBlockPrefabBounds = br.GetComponentInChildren<Collider>().bounds.size;
         smallBreakableBlockPrefabBounds = smbr.GetComponentInChildren<Collider>().bounds.size;
-        originalBlockLocalScale = unb.transform.localScale;
         Destroy(unb);
         Destroy(br);
         Destroy(smbr);
     }
-    
-    private void ListOfGroundsInit(){
+
+    private void ListOfGroundsInit()
+    {
         // Create list of list of size rowCount
         listOfGrounds = new List<List<GameObject>>(rowCount);
         // Create all respective sub-lists of size blockCount
@@ -96,47 +123,60 @@ public class SpawnManager : MonoBehaviour
         {
             for (int j = 0; j < blockCount; j++)
             {
-                if(j < (blockCount / 2)){
+                if (j < (blockCount / 2))
+                {
 
-                    if(j < (5 + (i+1) / 3) || (j > (blockCount/2) - (6 + (i+1) / 3))){
+                    if (j < (5 + (i + 1) / 3) || (j > (blockCount / 2) - (6 + (i + 1) / 3)))
+                    {
                         Vector3 spawnPos = new Vector3(originBlockPosition.x + j * unbreakableBlockPrefabBounds.x, originBlockPosition.y + i * rowHeight, originBlockPosition.z);
                         listOfGrounds[i].Add(Instantiate(unbreakableBlockPrefab, spawnPos, unbreakableBlockPrefab.transform.rotation));
                     }
-                    else{
+                    else
+                    {
                         Vector3 spawnPos = new Vector3(originBlockPosition.x + j * breakableBlockPrefabBounds.x, originBlockPosition.y + i * rowHeight, originBlockPosition.z);
                         listOfGrounds[i].Add(Instantiate(breakableBlockPrefab, spawnPos, breakableBlockPrefab.transform.rotation));
                     }
                 }
-                else if(i > 0){
+                else if (i > 0)
+                {
                     int randomIndex;
-                    if(listOfGrounds[i][j-1].CompareTag("Ground Breakable")){
+                    if (listOfGrounds[i][j - 1].CompareTag("Ground Breakable"))
+                    {
                         randomIndex = Random.Range(0, breakableBlocksPrefabs.Count);
                     }
-                    else if(listOfGrounds[i][j-1].CompareTag("Ground Breakable Small") && listOfGrounds[i][j-2].CompareTag("Ground Breakable")){
+                    else if (listOfGrounds[i][j - 1].CompareTag("Ground Breakable Small") && listOfGrounds[i][j - 2].CompareTag("Ground Breakable"))
+                    {
                         randomIndex = breakableBlocksPrefabs.Count;
                     }
-                    else if(listOfGrounds[i][j-1].CompareTag("Ground Breakable Small")){
+                    else if (listOfGrounds[i][j - 1].CompareTag("Ground Breakable Small"))
+                    {
                         randomIndex = breakableBlocksPrefabs.FindIndex(elem => elem.CompareTag("Ground Breakable"));
                     }
-                    else{
-                        if(Random.Range(0, 2) == 0){
+                    else
+                    {
+                        if (Random.Range(0, 2) == 0)
+                        {
                             randomIndex = breakableBlocksPrefabs.FindIndex(elem => elem.CompareTag("Ground Breakable Small"));
                         }
-                        else{
+                        else
+                        {
                             randomIndex = breakableBlocksPrefabs.Count;
                         }
-                        
+
                     }
-                    if(randomIndex != breakableBlocksPrefabs.Count){
-                        Vector3 spawnPos = new Vector3(originBlockPosition.x + (j - blockCount/2) * breakableBlocksPrefabsBounds[randomIndex].x, originBlockPosition.y + i * rowHeight - breakableBlocksPrefabsBounds[randomIndex].y - ((unbreakableBlockPrefabBounds.y - breakableBlocksPrefabsBounds[randomIndex].y) / 2), originBlockPosition.z);
+                    if (randomIndex != breakableBlocksPrefabs.Count)
+                    {
+                        Vector3 spawnPos = new Vector3(originBlockPosition.x + (j - blockCount / 2) * breakableBlocksPrefabsBounds[randomIndex].x, originBlockPosition.y + i * rowHeight - breakableBlocksPrefabsBounds[randomIndex].y - ((unbreakableBlockPrefabBounds.y - breakableBlocksPrefabsBounds[randomIndex].y) / 2), originBlockPosition.z);
                         listOfGrounds[i].Add(Instantiate(breakableBlocksPrefabs[randomIndex], spawnPos, breakableBlocksPrefabs[randomIndex].transform.rotation));
                     }
-                    else{
-                        Vector3 spawnPos = new Vector3(originBlockPosition.x + (j - blockCount/2) * unbreakableBlockPrefabBounds.x, originBlockPosition.y + i * rowHeight - unbreakableBlockPrefabBounds.y, originBlockPosition.z);
+                    else
+                    {
+                        Vector3 spawnPos = new Vector3(originBlockPosition.x + (j - blockCount / 2) * unbreakableBlockPrefabBounds.x, originBlockPosition.y + i * rowHeight - unbreakableBlockPrefabBounds.y, originBlockPosition.z);
                         listOfGrounds[i].Add(Instantiate(unbreakableBlockPrefab, spawnPos, unbreakableBlockPrefab.transform.rotation));
                         listOfGrounds[i][j].SetActive(false);
                     }
-                    if(j < (5 + (i+1) / 3 + blockCount/2)  || (j > blockCount - (6 + (i+1) / 3))){
+                    if (j < (5 + (i + 1) / 3 + blockCount / 2) || (j > blockCount - (6 + (i + 1) / 3)))
+                    {
                         listOfGrounds[i][j].SetActive(false);
                     }
                 }
